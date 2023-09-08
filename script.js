c6c49bb2b4edbbbe5abf70c06eeea844d321c902
rafhael <rafhaelmilanes12@gmail.com>
2023-09-07 21:08:49 -0300

    Inicio do Projeto SublimeLinks

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("ligth")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAtribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAtribute("src", "./assets/Avatar.png")
+  }
+}
